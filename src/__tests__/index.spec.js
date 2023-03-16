8f68025ec0e5ae81d51cf43f5a512eb6abda6267
hebertcisco <hebertcisco@outlook.com>
2023-03-16 13:34:40 -0300

    test: update src/__tests__/index.spec.js

--- src/__tests__/index.spec.js
+++ src/__tests__/index.spec.js
@@ -1,6 +1,6 @@
 const { sum } = require("../sum");
 
-describe("Soma de dois números", () => {
+describe("Teste de adição de números", () => {
     it("Deve retornar um número com a soma dos dois números informados", () => {
         const result = sum({ firstNumber: 1, secondNumber: 2 });
         expect(result).toBe(3);
